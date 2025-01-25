8768ed1caf93d24ced123e9edfa90da80437be08
Carlos Fernandes <carferdas@gmail.com>
2025-01-24 23:33:28 -0300

    add doctrine packages

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpKernel\Attribute\AsController;
 use Symfony\Component\Routing\Attribute\Route;
@@ -12,8 +13,10 @@ use Symfony\Component\Routing\Attribute\Route;
 class HomeController
 {
     #[Route('/', name: 'app_home', methods: ['GET'])]
-    public function index(): JsonResponse
+    public function index(EntityManagerInterface $entityManager): JsonResponse
     {
+        dd($entityManager->getConnection()->connect());
+
         return new JsonResponse([
             'message' => 'Hello from Docker',
         ]);
